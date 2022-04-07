aa0d0d67161bba7a92de0e702d6cad7857d6111a
Harry Penlington <harry.penlington@gmail.com>
2022-04-07 14:42:30 +0100

    come on. please.

--- src/lib/db.js
+++ src/lib/db.js
@@ -24,7 +24,7 @@ export function writeProductData(id, product) {
 }
 
 export function readProductData(id) {
-  const productRef = ref(database, `products/${id - 1}`);
+  const productRef = ref(database, `products/${id}`);
   get(productRef)
     .then((snapshot) => {
       const data = snapshot.exists() ? snapshot.val() : "No data available";
